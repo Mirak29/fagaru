534868fd59673f8093a0e5665d7484738af0ea5f
Mirak29 <abdoukarimsow363@gmail.com>
2024-10-15 17:57:24 +0000

    Storing and Retrieving files in IPFS using pinata

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
+import axios from 'axios';
 
 function App() {
   const [account, setAccount] = useState('');
+  const [file, setFile] = useState(null);
+  const [cid, setCid] = useState('');
 
+  // Connecter MetaMask
   useEffect(() => {
     const connectToMetaMask = async () => {
       if (window.ethereum) {
         try {
           const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-          setAccount(accounts[0]); // Définit le compte connecté
+          setAccount(accounts[0]);
         } catch (error) {
           console.error("User denied account access");
         }
@@ -18,14 +22,54 @@ function App() {
       }
     };
 
-    connectToMetaMask(); // Appel de la fonction pour se connecter à MetaMask
+    connectToMetaMask();
   }, []);
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    try {
+      const fileData = new FormData();
+      fileData.append("file", file);
+
+      const responeData = await axios({
+        method: "post",
+        url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        data: fileData,
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "pinata_api_key": process.env.REACT_APP_PINATA_API_KEY,
+          "pinata_secret_api_key": process.env.REACT_APP_PINATA_SECRET_KEY,
+        }
+      })
+      console.log("https://olive-many-canidae-360.mypinata.cloud/ipfs/" + responeData.data.IpfsHash);
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+  // Gestion du changement de fichier
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  // // Gérer l'upload
+  // const handleUpload = async () => {
+  //   if (file) {
+  //     await handleSubmit(file);
+  //   }
+  // };
 
   return (
     <div>
       <h1>Medical Records on Blockchain</h1>
       {account ? (
-        <p>Connected account: {account}</p>
+        <>
+          <p>Connected account: {account}</p>
+          <input type="file" onChange={handleFileChange} />
+          <button onClick={handleSubmit}>Upload to Pinata</button>
+          {cid && <p>File uploaded successfully. CID: {cid}</p>}
+        </>
       ) : (
         <p>Please connect to MetaMask</p>
       )}
@@ -34,3 +78,4 @@ function App() {
 }
 
 export default App;
+console.log(process.env.REACT_APP_PINATA_API_KEY);
